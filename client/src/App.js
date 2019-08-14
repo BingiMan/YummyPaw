fac21a22044e425c9aa3df393de592e0d305b9ad
bingi <luis.carbonel.gk@hotmail.com>
2019-08-14 18:17:36 -0400

    added routes and methods to create pets

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { withRouter } from 'react-router'
+import { Route, Link, withRouter } from 'react-router-dom'
 import Navigation from './components/header/Navigation'
+import Categories from './components/main/Categories'
+import Comment from './components/main/Comment'
 import { loginUser, verifyUser, createUser } from './services/user'
 import decode from 'jwt-decode';
 import './App.css';
+import { createPet } from './services/pet';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      // ----LOGIN-----
       loginForm: {
         username: '',
         password: '',
@@ -20,6 +24,21 @@ class App extends React.Component {
       },
       currentUser: null,
       loggedIn: null,
+      // ----E N D-----
+      // ----PETS-----
+      pets: [],
+      pets_form: {
+        title: '',
+        video_url: '',
+        is_cat: null,
+      },
+      currentPet: {
+        id: '',
+        title: '',
+        video_url: '',
+        is_cat:'',
+      }
+      // ----E N D-----
     }
   }
   handleRedirect = async (e) => {
@@ -69,58 +88,108 @@ class App extends React.Component {
     this.setState({
       currentUser: null
     })
-  } 
+  }
 
-//  ----------- REGISTER -------------
-  
-handleRegisterChange = (e) => {
-  const { target: { name, value } } = e;
-  this.setState(prevState => ({
-    registerForm: {
-      ...prevState.registerForm,
-      [name]: value,
-    }
-  }))
-}
-handleLogin = async () => {
-  const userData = await loginUser(this.state.authFormData);
-  this.setState({
-    currentUser: decode(userData.token)
-  })
-  localStorage.setItem("jwt", userData.token)
-}
-handleRegisterSubmit = async (e) => {
-  e.preventDefault();
-  await createUser(this.state.registerForm);
-  this.handleLogin();
-  this.setState({
-    registerForm: {
-      username: '',
-      email: '',
-      password: '',
-    }
-  })
-  // console.log(this.state.registerForm)
-}
+  //  ----------- REGISTER -------------
+
+  handleRegisterChange = (e) => {
+    const { target: { name, value } } = e;
+    this.setState(prevState => ({
+      registerForm: {
+        ...prevState.registerForm,
+        [name]: value,
+      }
+    }))
+  }
+  handleLogin = async () => {
+    const userData = await loginUser(this.state.authFormData);
+    this.setState({
+      currentUser: decode(userData.token)
+    })
+    localStorage.setItem("jwt", userData.token)
+  }
+  handleRegisterSubmit = async (e) => {
+    e.preventDefault();
+    await createUser(this.state.registerForm);
+    this.handleLogin();
+    this.setState({
+      registerForm: {
+        username: '',
+        email: '',
+        password: '',
+      }
+    })
+  }
+
+  // ------------------------END LOGIN- REGISTER-------------------------
+  handlePetChange = (e) => {
+    const { target: { name, value } } = e;
+    this.setState(prevState => ({
+      pets_form: {
+        ...prevState.pets_form,
+        [name] : value
+      }
+    }))
+  }
+
+  handlePetSubmit = async (ev) => {
+    ev.preventDefault();
+    const pets = await createPet(this.state.pets_form)
+    this.setState(prevState => ({
+      pets_form: [...prevState.pets_form, pets],
+      pets_form: {
+        title: '',
+        video_url: '',
+        is_cat: null,
+      },
+    }));
+    this.props.history.push(`/pets`)
+  }
+  // ------------------------END ADDING A PET-------------------------
+
+  handleDetail = (id) => {
+    this.setState(prevState => ({
+      currentPet: {
+        ...prevState.currentPet,
+        id:id,
+      }
+    }))
+  }
 
   render() {
     return (
       <div className="App">
         <header>
-        <Navigation
-          handleRedirect={this.handleRedirect}
-          currentUser={this.state.currentUser}
-          loggedIn={this.state.loggedIn}
-          handleLogout={this.handleLogout}
-          handleLoginChange={this.handleLoginChange}
-          handleLoginSubmit={this.handleLoginSubmit}
-          loginForm={this.state.loginForm}
+          <Navigation
+            handleRedirect={this.handleRedirect}
+            currentUser={this.state.currentUser}
+            loggedIn={this.state.loggedIn}
+            handleLogout={this.handleLogout}
+            handleLoginChange={this.handleLoginChange}
+            handleLoginSubmit={this.handleLoginSubmit}
+            loginForm={this.state.loginForm}
 
-          handleRegisterSubmit={this.handleRegisterSubmit}
-          handleRegisterChange={this.handleRegisterChange}
-          registerForm={this.state.registerForm}
+            handleRegisterSubmit={this.handleRegisterSubmit}
+            handleRegisterChange={this.handleRegisterChange}
+            registerForm={this.state.registerForm}
           />
-          </header>
+        </header>
+        <main>
+          <Route exact path="/pets/:id" render={() =>
+            <Categories
+              pets_form={this.state.pets}
+              handlePetSubmit={this.handlePetSubmit}
+              handlePetChange={this.handlePetChange}
+              handleDetail={this.handleDetail}
+            />} />
+          <Route exact path="/pets/:id/comment" render={() =>
+            <Comment
+            
+            />} />
+        </main>
+        <footer>
+
+        </footer>
       </div>
     );
   }
--- client/src/components/main/Categories.jsx
+++ client/src/components/main/Categories.jsx
@@ -5,6 +5,7 @@ export default class Categories extends React.Component {
     super()
 
   }
+  
 
 
   render() {
